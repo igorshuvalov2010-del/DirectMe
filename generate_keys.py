0531b3a46c2646d5f888f2a869d7d3d478840f0d
igorshuvalov2010-del <igorshuvalov2010@gmail.com>
2026-08-07 12:54:04 +0700

    Update generate_keys.py

--- generate_keys.py
+++ generate_keys.py
@@ -1,1 +1,10 @@
+from cryptography.fernet import Fernet
+import secrets
+import string
 
+def generate_secret_key():
+    alphabet = string.ascii_letters + string.digits + "!@#$%^&*()"
+    return ''.join(secrets.choice(alphabet) for _ in range(50))
+
+print(f"SECRET_KEY={generate_secret_key()}")
+print(f"ENCRYPTION_KEY={Fernet.generate_key().decode()}")
